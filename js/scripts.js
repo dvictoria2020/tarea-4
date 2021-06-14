5322cca5f7643bb71679a60085b6c1a22f2febff
dvictoria2020 <vicdelfer@gmail.com>
2021-06-14 17:20:27 -0600

    commit final

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Mapa Leaflet
-var mapa = L.map('mapid').setView([9.95, -84.15], 13);
+var mapa = L.map('mapid').setView([9.93, -84.12], 13.5);
 
 // Definición de capas base
 var capa_osm = L.tileLayer(
@@ -32,7 +32,7 @@ const iconoVivienda = L.divIcon({
 
 // Ícono personalizado para áreas públicas
 const iconoParques = L.divIcon({
-  html: '<i class="fas fa-thumbtack fa-2x"></i>',
+  html: '<i class="fas fa-thumbtack fa"></i>',
   className: 'estiloIconos'
 });
 	    
@@ -66,16 +66,16 @@ $.getJSON("https://dvictoria2020.github.io/tarea-4/datos/asentamientos_informale
 
   // Se añade la capa al mapa y al control de capas
   asentamientos_informales_agrupados.addTo(mapa);
-  control_capas.addOverlay(asentamientos_informales_agrupados, 'Asentamientos informales agrupados');
-  control_capas.addOverlay(capa_asentamientos_informales, 'Asentamientos informales individuales');
+  control_capas.addOverlay(asentamientos_informales_agrupados, 'Registros agrupados de asentamientos informales agrupados');
+  control_capas.addOverlay(capa_asentamientos_informales, 'Registros individuales de asentamientos informales individuales');
 });
 
 // Capa vectorial de registros áreas públicas
 $.getJSON("https://dvictoria2020.github.io/tarea-4/datos/areas_publicas.geojson", function(geodata) {
   //Registros individuales
-  var capa_areas_publicas_calor = L.geoJson(geodata, {
+  var capa_areas_publicas = L.geoJson(geodata, {
     style: function() {
-	  return {'color': "#013457", 'weight': 1}
+	  return {'color': "green", 'weight': 0.5}
     },
     onEachFeature: function(feature, layer) {
       var popupText = "<strong>Nombre</strong>: " + feature.properties.nomb_refere + "<br>" +
@@ -89,9 +89,11 @@ $.getJSON("https://dvictoria2020.github.io/tarea-4/datos/areas_publicas.geojson"
 
   // Capa de calor (heatmap)
   coordenadas = geodata.features.map(feat => feat.geometry.coordinates.reverse());
-  var capa_areas_publicas_calor = L.heatLayer(coordenadas, {radius: 30, blur: 15, gradient: {0.5: 'purple', 0.8: 'green', 1: 'red'}});  
+  var capa_areas_publicas_calor = L.heatLayer(coordenadas, {radius: 30, blur: 10, gradient: {0.5: 'purple', 0.8: 'green', 1: 'red'}});  
   
   // Se añaden las capas al mapa y al control de capas
   capa_areas_publicas_calor.addTo(mapa);
   control_capas.addOverlay(capa_areas_publicas_calor, 'Mapa de calor de las áreas públicas');
+  // capa_areas_publicas.addTo(mapa);
+  control_capas.addOverlay(capa_areas_publicas, 'Registros individuales de áreas públicas');
 });
